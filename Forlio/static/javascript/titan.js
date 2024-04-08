5a2cc882ab0b7dac7e966a0f0cf49ad0e901f039
binarymusa <finicky9819@gmail.com>
2024-04-09 00:28:30 +0300

    modified both pages heavily

--- Forlio/static/javascript/titan.js
+++ Forlio/static/javascript/titan.js
@@ -2,11 +2,20 @@
 
 window.addEventListener('scroll', function (){
     var chatIcon = document.querySelector('.chat-us');
+    var sideBar1 = document.querySelector('#fix')
+    var sideBar2 = document.querySelector('#fix2')
 
     if (window.scrollY > 100) {
       chatIcon.style.bottom = '20px';
+      sideBar1.style.top = '0px'
+      sideBar2.style.top = '0px';
+
     } else {
       chatIcon.style.bottom = '';
+      sideBar1.style.top = '';
+      sideBar2.style.top = '';
     }
+
 })
+
 
